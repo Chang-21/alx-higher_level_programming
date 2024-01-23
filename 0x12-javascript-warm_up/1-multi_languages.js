8f665ccfbf7058ed45e07e6846ba5fc649b84917
chang <changkuoth80@gmail.com>
2024-01-23 06:10:58 -0800

    solved a bug

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-console.log('C is fun\n');
-console.log('Python is cool\n');
-console.log('JavaScript is amazing');
+let message1 = "C is fun";
+let message2 = "Python is cool";
+let message3 = "JavaScript is amazing";
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
+
